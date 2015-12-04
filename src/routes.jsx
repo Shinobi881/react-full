7a05e0d477a83e5dcd3aef5a3a4f198a9b89d474
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2015-12-03 22:59:34 -0800

    Add fetch library and Api call

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,14 +4,12 @@ var HashHistory = require('react-router/lib/hashhistory');
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 
+var Main = require('./components/main');
+
 module.exports = (
   <Router history={new HashHistory}>
-    <Route path="/" component={Hello}>
-      <Route path="1" component={Child1}>
-        <Route path="2" component={Child2}/>
-
-      </Route>
-      <Route path="2" component={Child2}/>
+    <Route path="/" component={Main}>
+    
     </Route>
   </Router>
 )
