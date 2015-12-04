71dd356ec1524e9edb283bab220cf58e230767b2
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2015-12-04 00:45:32 -0800

    (style) Clean up and a little style

--- src/components/topic-list.jsx
+++ src/components/topic-list.jsx
@@ -1,6 +1,9 @@
 var React = require('react');
 var TopicStore = require('../stores/topic-store');
 var Reflux = require('reflux');
+var Actions = require('../actions');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 module.exports = React.createClass({
   mixins: [
@@ -12,7 +15,7 @@ module.exports = React.createClass({
     }
   },
   componentWillMount: function() {
-    TopicStore.getTopics();
+    Actions.getTopics();
   },
   render: function() {
     return <div className="list-group">
@@ -21,9 +24,10 @@ module.exports = React.createClass({
   },
   renderTopics: function() {
     return this.state.topics.map(function(topic) {
-      return <li>
-        {topic}
-      </li>
+      return <Link className="list-group-item" key={topic.id}>
+        <h4>{topic.name}</h4>
+        <p>{topic.description}</p>
+      </Link>
     })
   },
   onChange: function(event, topic) {
