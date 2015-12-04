a0d767f0b57899286fe8352fdefe92f6c8231999
Muhsin Abdul-Musawwir <moose.techguy@gmail.com>
2015-12-04 02:06:17 -0800

    (feat) Create app routed to each topic

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,10 +5,13 @@ var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 
 var Main = require('./components/main');
+var Topic = require('./components/topic')
 
 module.exports = (
   <Router history={new HashHistory}>
     <Route path="/" component={Main}>
+      <Route path="topics/:id" component={Topic}>
+      </Route>
     
     </Route>
   </Router>
